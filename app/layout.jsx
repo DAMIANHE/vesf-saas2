6017449f387991cc415697cfe6f9a2b71abdad93
DAMIANHE <66206811+DAMIANHE@users.noreply.github.com>
2025-12-16 12:09:00 -0300

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "VEFS DEMO",
+  description: "VEFS Demo 10 días"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+✔ Root 
