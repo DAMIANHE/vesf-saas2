2ab2c9e6c04d3dc010270f8956efd42a46161eca
DAMIANHE <66206811+DAMIANHE@users.noreply.github.com>
2025-12-16 12:10:37 -0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>VEFS</h1>
+      <p>Demo 10 días</p>
+
+      <Link href="/demo">
+        Ir al DEMO
+      </Link>
+    </main>
+  );
+}
